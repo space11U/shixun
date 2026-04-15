8dbb620c32bf687b16256d9e5e56f3fc0dbf7ce8
space11U <3161532866@qq.com>
2026-04-15 18:57:28 +0800

    Add files via upload

---
+++ widget.cpp
@@ -0,0 +1,29 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+#include <QDebug>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+
+void Widget::on_pushButton_clicked()
+{
+    if(ui->pushButton->text() == "开灯"){
+        myled.on(fsmpLeds::LED1);
+        ui->pushButton->setText("关灯");
+    }else{
+        myled.off(fsmpLeds::LED1);
+        ui->pushButton->setText("开灯");
+    }
+}
+
